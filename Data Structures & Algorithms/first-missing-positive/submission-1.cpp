e1c4caaeb000f2c04ff6c5877edd5413f358481a
Rohan J <81848778+Mindwrap-Error@users.noreply.github.com>
2026-04-05 20:01:20 +0530

    Add: first-missing-positive - submission-1

---
+++ Data Structures & Algorithms/first-missing-positive/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        map<int, bool> isPresent;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            isPresent[nums[i]] = true;
+        }
+
+        for(int i = 1; i < INT_MAX; i++)
+        {
+            if(isPresent[i] == false)
+            {
+                return i;
+            }
+        }
+    }
+};
