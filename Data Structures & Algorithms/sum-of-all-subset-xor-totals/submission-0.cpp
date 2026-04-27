c4e616c492aa9fc4b58671d199ad8320128cc95f
Rohan J <81848778+Mindwrap-Error@users.noreply.github.com>
2026-04-27 23:48:48 +0530

    Add: sum-of-all-subset-xor-totals - submission-0

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int res = 0;
+        for(int& num: nums)
+        {
+            res |= num;
+        }
+        return res << (nums.size() - 1);
+    }
+};
