8f71e8df1ed9d462969bf71e11fd7e81f736b75e
Rohan J <81848778+Mindwrap-Error@users.noreply.github.com>
2026-05-06 20:39:53 +0530

    Add: merge-sorted-array - submission-1

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = n + m; i >= n; i--)
+        {
+            nums1[i] = nums1[i - n];
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            nums1[i] = nums2[i];
+        }
+
+        sort(nums1.begin(), nums1.end());
+    }
+};
