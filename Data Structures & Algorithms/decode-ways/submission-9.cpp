9dcedf6933506dae4d7d4655815ca6ed079b3b01
Rohan J <81848778+Mindwrap-Error@users.noreply.github.com>
2026-05-24 16:48:01 +0530

    Add: decode-ways - submission-9

---
+++ Data Structures & Algorithms/decode-ways/submission-9.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if(s[0] == '0') return 0;
+
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = (s[0] == '0' ? 0 : 1);
+
+        for(int i = 2; i <= s.size(); i++)
+        {
+            if(s[i-1] != '0') dp[i] += dp[i-1];
+            if(s.substr(i-2, 2) >= "10" && s.substr(i-2, 2) <= "26") dp[i] += dp[i-2];
+        }
+        return dp[n];
+    }
+};
